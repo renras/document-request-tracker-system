cdfce5c3ba173615bacac093a32a3751903f0466
Renzo Visperas <renzovisperas07@gmail.com>
2022-09-30 21:53:51 +0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button>Button</button>
+    </div>
+  );
 }
 
 export default App;
