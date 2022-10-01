6441b02cd929cd4a4fec7474b52456c22c2b620d
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-01 20:58:40 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <button>Button</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <button>Home</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
