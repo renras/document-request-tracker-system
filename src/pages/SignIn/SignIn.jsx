3bf7c7a8245b83ea03b41f5c129769b66e522835
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-04 18:09:13 +0800

    add sign in form

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,43 @@
+import styles from "./SignIn.module.css";
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <div className={styles.container}>
+      <form className="mw-sm border w-100 shadow-sm rounded py-5 px-4">
+        <h1 className="h3">Sign In</h1>
+
+        {/* email */}
+        <label className="form-label mt-4" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="form-control form-control-lg"
+          id="email"
+          type="email"
+        />
+
+        {/* password */}
+        <label className="form-label mt-3" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="form-control form-control-lg"
+          id="password"
+          type="password"
+        />
+        <button className="btn btn-primary btn-lg w-100 mt-5">Sign In</button>
+
+        {/* sign up link */}
+        <div className="text-center mt-4">
+          Do not have an account yet?
+          <Link to="/sign-up" className="ms-2">
+            Sign up
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
