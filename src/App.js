7bacc6da9f15621afbe67dcf007e0aa1a416c44c
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-04 17:50:13 +0800

    initialize routes to sign, signup, dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
