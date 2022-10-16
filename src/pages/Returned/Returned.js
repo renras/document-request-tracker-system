3a4aa29a1050d3cf305fdd5d72b22f5b275a5430
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-16 10:47:53 +0800

    update return page

--- src/pages/Returned/Returned.js
+++ src/pages/Returned/Returned.js
@@ -1,9 +1,31 @@
 import SignedInLayout from "../../components/Layouts/SignedInLayout/SignedInLayout";
-import documentsData from "../../documents.json";
 import AdminDocumentsTable from "../../components/AdminDocumentsTable/AdminDocumentsTable";
+import { collection } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../firebase-config";
 
 const Returned = () => {
-  const documents = documentsData.data.filter(
+  const [documentsData, documentsDataLoading, documentsDataError] =
+    useCollection(collection(db, "documents"));
+  const [usersData, usersDataLoading, usersDataError] = useCollection(
+    collection(db, "users")
+  );
+
+  if ((usersDataLoading, documentsDataLoading)) return <div>Loading...</div>;
+  if ((usersDataError, documentsDataError))
+    return <div>Failed to load page...</div>;
+
+  const users = usersData.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
+
+  const documents = documentsData.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+    author: users.find((user) => user.id === doc.data().authorId),
+  }));
+
+  const returnedDocuments = documents.filter(
     (document) => document.status === "RETURNED"
   );
 
@@ -11,7 +33,7 @@ const Returned = () => {
     <SignedInLayout>
       <div className="px-4">
         <h1 className="h2">Returned</h1>
-        <AdminDocumentsTable documents={documents} />
+        <AdminDocumentsTable documents={returnedDocuments} />
       </div>
     </SignedInLayout>
   );
