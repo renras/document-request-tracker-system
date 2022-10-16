3bb98149b504313412e11854c26d3e884821ca23
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-16 10:42:40 +0800

    add accept and reject document handlers

--- src/components/AdminDocumentsTable/AdminDocumentsTable.js
+++ src/components/AdminDocumentsTable/AdminDocumentsTable.js
@@ -1,15 +1,33 @@
 import PropTypes from "prop-types";
 import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
-const columns = [
-  "Tracking Number",
-  "Document",
-  "Type",
-  "Requested By",
-  "Action",
-];
+const columns = ["Tracking ID", "Document", "Type", "Requested By", "Action"];
 
 const Table = ({ documents }) => {
+  const handleAcceptDocument = async (id) => {
+    try {
+      const docRef = doc(db, "documents", id);
+      await updateDoc(docRef, {
+        status: "RECEIVED",
+      });
+    } catch {
+      alert("Failed to receive document. Please try again later.");
+    }
+  };
+
+  const handleReturnDocument = async (id) => {
+    try {
+      const docRef = doc(db, "documents", id);
+      await updateDoc(docRef, {
+        status: "RETURNED",
+      });
+    } catch {
+      alert("Failed to return document. Please try again later.");
+    }
+  };
+
   return (
     <table className="table mt-5 align-middle">
       <thead>
@@ -34,10 +52,16 @@ const Table = ({ documents }) => {
               {status === "INCOMING" && (
                 <td>
                   <div className="d-flex gap-2">
-                    <button className="btn btn-sm btn-light text-success">
+                    <button
+                      className="btn btn-sm btn-light text-success"
+                      onClick={() => handleAcceptDocument(id)}
+                    >
                       <AiFillCheckCircle size={24} />
                     </button>
-                    <button className="btn btn-sm btn-light text-danger">
+                    <button
+                      className="btn btn-sm btn-light text-danger"
+                      onClick={() => handleReturnDocument(id)}
+                    >
                       <AiFillCloseCircle size={24} />
                     </button>
                   </div>
