12b72362dc09a0f19c029357ecd5d1521ea86079
brianblip <67305006+brianblip@users.noreply.github.com>
2022-10-18 17:32:50 +0800

    Update profile (#19)
    * Update Profile
    
    Added the update system when they save their changes in profile setting
    
    * Updated the ContactUs
    
    * Update ContactUs
    
    Co-authored-by: Renzo Visperas <renzovisperas07@gmail.com>

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -2,18 +2,17 @@ import SignedOutLayout from "../../components/Layouts/SignedOutLayout/SignedOutL
 import { db } from "../../firebase-config";
 import { addDoc, collection } from "firebase/firestore";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const ContactUs = () => {
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
   const onSubmit = async (data) => {
     try {
-      await addDoc(collection(db, "messages"), {
-        name: data.name,
-        email: data.email,
-        message: data.message,
-      });
+      await addDoc(collection(db, "messages"), data);
       alert("Message sent successfully");
+      navigate(0);
     } catch {
       alert("Failed to send message. Please try again later.");
     }
@@ -28,16 +27,31 @@ const ContactUs = () => {
         >
           <h2 className="h3 ">Contact us</h2>
 
-          {/* name */}
-          <label className="form-label mt-4" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="form-control"
-            type="text"
-            id="name"
-            {...register("name", { required: true })}
-          />
+          {/* add first name and last name */}
+          <div className="row">
+            <div className="col">
+              <label className="form-label mt-3" htmlFor="firstname">
+                First Name
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="firstname"
+                {...register("fistname", { required: true })}
+              />
+            </div>
+            <div className="col">
+              <label className="form-label mt-3" htmlFor="lastname">
+                Last Name
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="lastname"
+                {...register("lastname", { required: true })}
+              />
+            </div>
+          </div>
 
           {/* email */}
           <label className="form-label mt-3" htmlFor="email">
@@ -48,6 +62,16 @@ const ContactUs = () => {
             type="email"
             id="email"
             {...register("email", { required: true })}
+          />
+
+          {/* Title */}
+          <label className="form-label mt-3" htmlFor="topic">
+            Topic
+          </label>
+          <input
+            className="form-control"
+            id="topic"
+            {...register("topic", { required: true })}
           />
 
           {/* message */}
