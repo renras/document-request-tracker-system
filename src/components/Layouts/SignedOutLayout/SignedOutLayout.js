442f03432431149cb0de8ae3b34c4ed3cef598d6
Renzo Visperas <85605271+renras@users.noreply.github.com>
2022-12-18 09:56:28 +0800

    remove redirect if user is already logged in in signed out page (#33)

--- src/components/Layouts/SignedOutLayout/SignedOutLayout.js
+++ src/components/Layouts/SignedOutLayout/SignedOutLayout.js
@@ -1,43 +1,15 @@
 import PropTypes from "prop-types";
 import styles from "./SignedOutLayout.module.css";
-import { auth } from "../../../firebase-config";
-import { onAuthStateChanged } from "firebase/auth";
-import { useNavigate, useLocation } from "react-router-dom";
-import { useEffect } from "react";
 import { Chatbot } from "react-chatbot-kit";
 import MessageParser from "../../../chatbot/MessageParser";
 import useConfig from "../../../chatbot/useConfig";
 import ActionProvider from "../../../chatbot/ActionProvider";
 import { BsChevronUp } from "react-icons/bs";
 import ChatBotHeader from "../../../chatbot/components/Header/Header";
-import { useState } from "react";
-import Loader from "../../Loader/Loader";
 import Header from "./Header/Header";
 
 const SignedOutLayout = ({ children }) => {
-  const navigate = useNavigate();
-  const location = useLocation();
   const [config, showBot, setShowBot] = useConfig();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (location.pathname.includes("sign-up")) {
-      setLoading(false);
-      return;
-    }
-
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        navigate("/documents");
-      }
-
-      setLoading(false);
-    });
-
-    return () => unsubscribe();
-  }, [location.pathname, navigate]);
-
-  if (loading) return <Loader />;
 
   return (
     <div className={styles.container}>
