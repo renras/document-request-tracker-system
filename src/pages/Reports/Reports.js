e03711205cadb27dfbaa1f34e379ad8584f57fde
Renzo Visperas <renzovisperas07@gmail.com>
2023-01-23 10:45:40 +0800

    add dropdown

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,17 +1,21 @@
 import SignedInLayout from "../../components/Layouts/SignedInLayout/SignedInLayout";
-import { BsSearch } from "react-icons/bs";
+import Dropdown from "../../components/ui/Dropdown/Dropdown";
+import { useState } from "react";
+import { DOCUMENT_TYPES } from "../Documents/CreateDocumentModal/documentTypes";
 
 const Reports = () => {
+  const [documentType, setDocumentType] = useState(DOCUMENT_TYPES[0]);
+
   return (
     <SignedInLayout>
       <div className="px-4">
         <h1 className="h2 mt-5">Reports</h1>
-        <div className="mt-5 position-relative" style={{ maxWidth: "384px " }}>
-          <input type="text" className="form-control" />
-
-          <BsSearch
-            className="position-absolute  top-50 translate-middle-y text-black-50"
-            style={{ right: "1rem" }}
+        <div className="mt-5 position-relative" style={{ maxWidth: "256px" }}>
+          <Dropdown
+            id="document-type"
+            value={documentType}
+            options={DOCUMENT_TYPES}
+            onChange={(option) => setDocumentType(option)}
           />
         </div>
       </div>
