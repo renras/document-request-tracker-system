d7aa3436f71ecae83fde7a3146fef0881346542c
Renzo Visperas <renzovisperas07@gmail.com>
2023-01-23 11:31:15 +0800

    fetch document type counts

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,10 +1,46 @@
 import SignedInLayout from "../../components/Layouts/SignedInLayout/SignedInLayout";
 import { DOCUMENT_TYPES } from "../Documents/CreateDocumentModal/documentTypes";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { db } from "../../firebase-config";
+import { useEffect } from "react";
 
 const Reports = () => {
   const documentTypes = DOCUMENT_TYPES.filter(
-    (document) => document.label !== "Choose document type"
+    (document) => document.value !== ""
   );
+
+  const getDocumentTypeCount = async (documentType) => {
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise(async (resolve, reject) => {
+      try {
+        const q = query(
+          collection(db, "documents"),
+          where("documentType", "==", documentType)
+        );
+        const querySnapshot = await getDocs(q);
+        const documentTypeCount = querySnapshot.size;
+        resolve({ data: documentTypeCount, error: null });
+      } catch (error) {
+        console.error(error);
+        reject({ data: null, error: "Failed to get document type count" });
+      }
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const documentTypeCounts = await Promise.all(
+        documentTypes.map(async (documentType) => {
+          const { data: requestsCount } = await getDocumentTypeCount(
+            documentType.value
+          );
+          return { requestsCount: requestsCount };
+        })
+      );
+      console.log(documentTypeCounts);
+    })();
+  }, [documentTypes]);
+
   return (
     <SignedInLayout>
       <div className="px-4">
@@ -18,8 +54,8 @@ const Reports = () => {
             </tr>
           </thead>
           <tbody>
-            {documentTypes.map((document) => (
-              <tr key={document}>
+            {documentTypes.map((document, index) => (
+              <tr key={index}>
                 <td>{document.label}</td>
                 <td>0</td>
                 <td>0</td>
