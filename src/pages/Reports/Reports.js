c8fcfaad51286b03b5641283917aaee46a5e0bab
Renzo Visperas <renzovisperas07@gmail.com>
2023-01-23 10:53:43 +0800

    create reports table

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,23 +1,32 @@
 import SignedInLayout from "../../components/Layouts/SignedInLayout/SignedInLayout";
-import Dropdown from "../../components/ui/Dropdown/Dropdown";
-import { useState } from "react";
 import { DOCUMENT_TYPES } from "../Documents/CreateDocumentModal/documentTypes";
 
 const Reports = () => {
-  const [documentType, setDocumentType] = useState(DOCUMENT_TYPES[0]);
-
+  const documentTypes = DOCUMENT_TYPES.filter(
+    (document) => document.label !== "Choose document type"
+  );
   return (
     <SignedInLayout>
       <div className="px-4">
         <h1 className="h2 mt-5">Reports</h1>
-        <div className="mt-5 position-relative" style={{ maxWidth: "256px" }}>
-          <Dropdown
-            id="document-type"
-            value={documentType}
-            options={DOCUMENT_TYPES}
-            onChange={(option) => setDocumentType(option)}
-          />
-        </div>
+        <table className="table mt-5">
+          <thead>
+            <tr className="table-success">
+              <th scope="col">Document Type</th>
+              <th scope="col">No. of Requests</th>
+              <th scope="col">No. of Completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {documentTypes.map((document) => (
+              <tr key={document}>
+                <td>{document.label}</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </SignedInLayout>
   );
