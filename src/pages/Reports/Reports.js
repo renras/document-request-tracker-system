57930300278bbd4a9a089b0c09c105337e798017
Renzo Visperas <85605271+renras@users.noreply.github.com>
2023-02-05 13:45:25 +0800

    Ren/add date picker reports (#54)
    * add react date picker
    
    * filter reports by date
    
    * make filter by date more exact
    
    ---------
    
    Co-authored-by: Renzo Visperas <renzoviperas07@gmail.com>

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -2,75 +2,110 @@ import SignedInLayout from "../../components/Layouts/SignedInLayout/SignedInLayo
 import { DOCUMENT_TYPES } from "../Documents/CreateDocumentModal/documentTypes";
 import { query, collection, getDocs, where } from "firebase/firestore";
 import { db } from "../../firebase-config";
-import { useEffect } from "react";
 import { useState } from "react";
 import Loader from "../../components/Loader/Loader";
 import Error from "../../components/Error/Error";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import useDeepCompareEffect from "use-deep-compare-effect";
+import { MdDateRange } from "react-icons/md";
 
 const Reports = () => {
-  const [reports, setReports] = useState(null);
+  const [reports, setReports] = useState([]);
   const [reportsLoading, setReportsLoading] = useState(true);
   const [reportsError, setReportsError] = useState(null);
+  const [date, setDate] = useState(null);
   const documentTypes = DOCUMENT_TYPES.filter(
     (document) => document.value !== ""
   );
 
-  const getDocumentTypeReport = async (documentType) => {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolve, reject) => {
-      try {
-        const qRequestsCount = await query(
-          collection(db, "documents"),
-          where("documentType", "==", documentType)
-        );
-        const qCompletedRequestsCount = query(
-          collection(db, "documents"),
-          where("documentType", "==", documentType),
-          where("status", "==", "RELEASED")
-        );
-
-        const querySnapshotRequestsCount = await getDocs(qRequestsCount);
-        const querySnapshotCompletedRequestsCount = await getDocs(
-          qCompletedRequestsCount
-        );
-        const documentTypeRequestsCount = querySnapshotRequestsCount.size;
-        const documentTypeCompletedRequestsCount =
-          querySnapshotCompletedRequestsCount.size;
-
-        resolve({
-          data: {
-            requestsCount: documentTypeRequestsCount,
-            completedRequestsCount: documentTypeCompletedRequestsCount,
-          },
-          error: null,
-        });
-      } catch (error) {
-        console.error(error);
-        reject({ data: null, error: "Failed to fetch reports" });
-      }
-    });
-  };
-
-  useEffect(() => {
-    if (!reports) {
-      (async () => {
+  useDeepCompareEffect(() => {
+    const getDocumentTypeReport = async (documentType) => {
+      // eslint-disable-next-line no-async-promise-executor
+      return new Promise(async (resolve, reject) => {
         try {
-          const documentTypeCounts = await Promise.all(
-            documentTypes.map(async (documentType) => {
-              const { data } = await getDocumentTypeReport(documentType.value);
-              return data;
-            })
+          const formatDate = moment(date).format("YYYY-MM-DD");
+          const dateYesterday = date
+            ? new Date(`${formatDate} 00:00:00`)
+            : null;
+
+          const dateTomorrow = date ? new Date(`${formatDate} 23:59:59`) : null;
+
+          const getQRequestsCount = () => {
+            if (!date)
+              return query(
+                collection(db, "documents"),
+                where("documentType", "==", documentType)
+              );
+
+            return query(
+              collection(db, "documents"),
+              where("documentType", "==", documentType),
+              where("createdAt", ">=", dateYesterday),
+              where("createdAt", "<=", dateTomorrow)
+            );
+          };
+
+          const getQCompletedRequestsCount = () => {
+            if (!date)
+              return query(
+                collection(db, "documents"),
+                where("documentType", "==", documentType),
+                where("status", "==", "RELEASED")
+              );
+
+            return query(
+              collection(db, "documents"),
+              where("documentType", "==", documentType),
+              where("status", "==", "RELEASED"),
+              where("documentType", "==", documentType),
+              where("createdAt", ">=", dateYesterday),
+              where("createdAt", "<=", dateTomorrow)
+            );
+          };
+
+          const qRequestsCount = getQRequestsCount();
+          const qCompletedRequestsCount = getQCompletedRequestsCount();
+
+          const querySnapshotRequestsCount = await getDocs(qRequestsCount);
+          const querySnapshotCompletedRequestsCount = await getDocs(
+            qCompletedRequestsCount
           );
-          setReports(documentTypeCounts);
+          const documentTypeRequestsCount = querySnapshotRequestsCount.size;
+          const documentTypeCompletedRequestsCount =
+            querySnapshotCompletedRequestsCount.size;
+
+          resolve({
+            data: {
+              requestsCount: documentTypeRequestsCount,
+              completedRequestsCount: documentTypeCompletedRequestsCount,
+            },
+            error: null,
+          });
         } catch (error) {
           console.error(error);
-          setReportsError("Failed to fetch reports");
-        } finally {
-          setReportsLoading(false);
+          reject({ data: null, error: "Failed to fetch reports" });
         }
-      })();
-    }
-  }, [documentTypes, reports]);
+      });
+    };
+
+    (async () => {
+      try {
+        const documentTypeCounts = await Promise.all(
+          documentTypes.map(async (documentType) => {
+            const { data } = await getDocumentTypeReport(documentType.value);
+            return data;
+          })
+        );
+        setReports(documentTypeCounts);
+      } catch (error) {
+        console.error(error);
+        setReportsError("Failed to fetch reports");
+      } finally {
+        setReportsLoading(false);
+      }
+    })();
+  }, [date, reports]);
 
   if (reportsLoading) return <Loader />;
   if (reportsError) return <Error />;
@@ -87,7 +122,22 @@ const Reports = () => {
     <SignedInLayout>
       <div className="px-4">
         <h1 className="h2 mt-5">Reports</h1>
-        <table className="table mt-5">
+        <div className="mt-5">
+          <div className="position-relative" style={{ maxWidth: "192px" }}>
+            <DatePicker
+              className="form-control"
+              selected={date}
+              onChange={(date) => setDate(date)}
+            />
+            <MdDateRange
+              className="position-absolute top-50 translate-middle-y"
+              size={20}
+              color="#6c757d"
+              style={{ right: "10px" }}
+            />
+          </div>
+        </div>
+        <table className="table mt-3">
           <thead>
             <tr className="table-success">
               <th scope="col">Document Type</th>
