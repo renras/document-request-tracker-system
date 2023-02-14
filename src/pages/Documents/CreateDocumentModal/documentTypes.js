01cba9303291808987f954f339e19f911ab48915
emzies <112775211+emzies@users.noreply.github.com>
2023-02-14 19:29:10 +0800

    added all lists of registrar's fees for Document Requests (#62)

--- src/pages/Documents/CreateDocumentModal/documentTypes.js
+++ src/pages/Documents/CreateDocumentModal/documentTypes.js
@@ -1,11 +1,123 @@
 export const DOCUMENT_TYPES = [
   {
+    label: "Processing Fee for 4Ps",
+    value: "Processing Fee for 4Ps",
+  },
+  {
+    label: "Processing Fee-Local",
+    value: "Processing Fee-Local",
+  },
+  {
+    label: "Processing Fee-Abroad(CGFNS, WES, etc.)",
+    value: "Processing Fee-Abroad(CGFNS, WES, etc.)",
+  },
+  {
+    label: "OTR W/ EDUFIED GRAD AS OF 2018 TO OLD WITH DOC STAMP (NEW RATE)",
+    value: "OTR W/ EDUFIED GRAD AS OF 2018 TO OLD WITH DOC STAMP (NEW RATE)",
+  },
+  {
+    label: "Diploma-Rush",
+    value: "Diploma-Rush",
+  },
+  {
+    label: "For 137/138",
+    value: "For 137/138",
+  },
+  {
+    label: "Form 137/138-Rush",
+    value: "Form 137/138-Rush",
+  },
+  {
+    label: "Trancript (first page with doc stamp)",
+    value: "Trancript (first page with doc stamp)",
+  },
+  {
+    label: "Transcript (suceeding page)",
+    value: "Transcript (suceeding page)",
+  },
+  {
+    label: "Transcript (Rush rate; per page)",
+    value: "Transcript (Rush rate; per page)",
+  },
+  {
+    label: "Authentication-Single",
+    value: "Authentication-Single",
+  },
+  {
+    label: "Authentication-Local",
+    value: "Authentication-Local",
+  },
+  {
+    label: "CAV (Authentication for CHED DFA Red Ribbon)",
+    value: "CAV (Authentication for CHED DFA Red Ribbon)",
+  },
+  {
+    label: "CAV with processing",
+    value: "CAV with processing",
+  },
+  {
+    label: "Copy of Grades",
+    value: "Copy of Grades",
+  },
+  {
+    label: "Dropping Fee/Changing (per subject)-Tertiary",
+    value: "Dropping Fee/Changing (per subject)-Tertiary",
+  },
+  {
+    label: "Dropping Fee/Changing )per subject)-Law/GS",
+    value: "Dropping Fee/Changing )per subject)-Law/GS",
+  },
+  {
+    label: "Print Out Assessment",
+    value: "Print Out Assessment",
+  },
+  {
+    label: "CHD - Grad School",
+    value: "CHD - Grad School",
+  },
+  {
+    label: "CHD - Law",
+    value: "CHD - Law",
+  },
+  {
+    label: "CHD - Tertiary",
+    value: "CHD - Tertiary",
+  },
+  {
+    label: "CHD - TechEd",
+    value: "CHD - TechEd",
+  },
+  {
+    label: "CHD - BasicEd",
+    value: "CHD - BasicEd",
+  },
+  {
+    label: "Certification - Course Description",
+    value: "Certification - Course Description",
+  },
+  {
+    label: "Certification - Letter of Acceptance",
+    value: "Certification - Letter of Acceptance",
+  },
+  {
+    label: "Certification - Letter of Acceptance; with SPA",
+    value: "Certification - Letter of Acceptance; with SPA",
+  },
+  {
     label: "Certification of Enrollment",
     value: "Certification of Enrollment",
   },
   {
     label: "Certification of Graduation",
     value: "Certification of Graduation",
+  },
+  {
+    label: "Certification of Units Earned",
+    value: "Certification of Units Earned",
+  },
+  {
+    label: "Certification-Med. of Instruction",
+    value: "Certification-Med. of Instruction",
   },
   {
     label: "Certification-Weighted Average",
@@ -16,15 +128,47 @@ export const DOCUMENT_TYPES = [
     value: "Certification-Good Moral",
   },
   {
-    label: "Certification-Good Moral",
-    value: "Certification-Good Moral",
+    label: "Certificate of Eligibility to Transfer, 2nd copy",
+    value: "Certificate of Eligibility to Transfer, 2nd copy",
   },
   {
-    label: "Certification-Good Moral",
-    value: "Certification-Good Moral",
+    label: "Change of Section-SHS",
+    value: "Change of Section-SHS",
   },
   {
-    label: "Certification-Good Moral",
-    value: "Certification-Good Moral",
+    label: "Completion Grade-Tertiary",
+    value: "Completion Grade-Tertiary",
+  },
+  {
+    label: "Completion Grade-Law/GS",
+    value: "Completion Grade-Law/GS",
+  },
+  {
+    label: "Certification - OTR w/ hrs",
+    value: "Certification - OTR w/ hrs",
+  },
+  {
+    label: "Certification of Assessment",
+    value: "Certification of Assessment",
+  },
+  {
+    label: "Forms (for education and/or employment)",
+    value: "Forms (for education and/or employment)",
+  },
+  {
+    label: "RLE (per page)",
+    value: "RLE (per page)",
+  },
+  {
+    label: "Docementary Stamps",
+    value: "Docementary Stamps",
+  },
+  {
+    label: "Course prospectus",
+    value: "Course prospectus",
+  },
+  {
+    label: "Processing of correction of name",
+    value: "Processing of correction of name",
   },
 ];
