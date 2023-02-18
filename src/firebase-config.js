5ef11206bb03d8d3681c457b95a3a88365ac8e44
Renzo Visperas <renzoviperas07@gmail.com>
2023-02-18 20:33:03 +0800

    add emulators

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,3 +26,9 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
+
+// connect to emulator
+connectFirestoreEmulator(db, "localhost", 8080);
+// connectauthemulator needs to have this format http://localhost:9099 or else it will throw an error
+connectAuthEmulator(auth, "http://localhost:9099");
+connectStorageEmulator(storage, "localhost", 9199);
