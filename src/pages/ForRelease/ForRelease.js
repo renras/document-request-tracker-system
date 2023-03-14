7ef425d30ef4b5ca26a2a0d513d8a5593c7fe612
Renzo Visperas <renzoviperas07@gmail.com>
2023-03-14 17:53:59 +0800

    add email js reason for rejection

--- src/pages/ForRelease/ForRelease.js
+++ src/pages/ForRelease/ForRelease.js
@@ -7,9 +7,10 @@ import { auth } from "../../firebase-config";
 import updateRequestType from "../../services/updateRequestType";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Modal from "../../components/v2/Modal/Modal";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { Timestamp } from "@firebase/firestore";
+import emailjs from "@emailjs/browser";
 
 const ForRelease = () => {
   const {
@@ -22,6 +23,11 @@ const ForRelease = () => {
   const { register, handleSubmit } = useForm();
   const [documentToReject, setDocumentToReject] = useState(null);
   const [showRejectModal, setShowRejectModal] = useState(false);
+  const {
+    register: rejectModalRegister,
+    handleSubmit: rejectModalHandleSubmit,
+  } = useForm();
+  const formRef = useRef(null);
 
   if (loading || userLoading) return <Loader />;
   if (error || userError) return <Error />;
@@ -43,6 +49,7 @@ const ForRelease = () => {
         "has released your request",
         newDate
       );
+
       setDocumentForRelease(null); // close the modal
     } catch (e) {
       console.error(e);
@@ -64,8 +71,15 @@ const ForRelease = () => {
         documentToReject.recipientId,
         "has rejected your request"
       );
+
+      await emailjs.sendForm(
+        "service_fs7z2n6",
+        "template_d92oyze",
+        formRef.current,
+        "nwbcoDLTBTvMU-vIp"
+      );
+
       setShowRejectModal(false);
-      alert("Document has been rejected.");
     } catch (e) {
       console.error(e);
       alert("Failed to reject document. Please try again later.");
@@ -98,26 +112,37 @@ const ForRelease = () => {
             onClick={() => setShowRejectModal(false)}
           ></button>
         </div>
-        <div className="modal-body">
-          <p>Are you sure you want to reject this request?</p>
-        </div>
-        <div className="modal-footer">
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-bs-dismiss="modal"
-            onClick={() => setShowRejectModal(false)}
-          >
-            Cancel
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={handleRejectConfirmation}
-          >
-            Reject
-          </button>
-        </div>
+        <form
+          ref={formRef}
+          onSubmit={rejectModalHandleSubmit(handleRejectConfirmation)}
+        >
+          <div className="modal-body">
+            <p>Are you sure you want to reject this request?</p>
+            <input
+              {...register("requestor_email")}
+              defaultValue={user.email}
+              hidden
+            />
+            <textarea
+              className="form-control"
+              aria-label="reason for rejection"
+              rows="5"
+              placeholder="Enter the reason here"
+              {...rejectModalRegister("reason", { required: true })}
+            />
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+              onClick={() => setShowRejectModal(false)}
+            >
+              Cancel
+            </button>
+            <button className="btn btn-danger">Reject</button>
+          </div>
+        </form>
       </Modal>
 
       {/* relase document  modal */}
